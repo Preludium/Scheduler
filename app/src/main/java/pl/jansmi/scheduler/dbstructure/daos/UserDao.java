6dbb17afb8cfbac78f8836798a44a457f375543c
jansmi <janek.smielowski@gmail.com>
2019-10-21 17:12:17 +0200

    Dao init

--- app/src/main/java/pl/jansmi/scheduler/dbstructure/daos/UserDao.java
+++ app/src/main/java/pl/jansmi/scheduler/dbstructure/daos/UserDao.java
@@ -1,4 +1,25 @@
 package pl.jansmi.scheduler.dbstructure.daos;
 
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import pl.jansmi.scheduler.dbstructure.entities.User;
+
+@Dao
 public interface UserDao {
+
+    @Query("SELECT * FROM Users WHERE name = :name")
+    User getByName(String name);
+
+    @Insert
+    void insert(User user);
+
+    @Update
+    void update(User user);
+
+    @Delete
+    void delete(User user);
 }
