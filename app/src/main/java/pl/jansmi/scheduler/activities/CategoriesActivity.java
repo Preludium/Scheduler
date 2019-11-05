92643c6e4e5836881cef1d5316c3169ab04e1b33
jansmi <janek.smielowski@gmail.com>
2019-11-06 00:03:12 +0100

    Functional categories listing & adding

--- app/src/main/java/pl/jansmi/scheduler/activities/CategoriesActivity.java
+++ app/src/main/java/pl/jansmi/scheduler/activities/CategoriesActivity.java
@@ -1,18 +1,25 @@
 package pl.jansmi.scheduler.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
 
 import pl.jansmi.scheduler.R;
+import pl.jansmi.scheduler.adapters.CategoriesRecyclerViewAdapter;
 
 public class CategoriesActivity extends AppCompatActivity {
+
+    private RecyclerView recycler;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +28,24 @@ public class CategoriesActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        recycler = findViewById(R.id.categories_content_recycler);
+
+        manager = new LinearLayoutManager(this);
+        recycler.setLayoutManager(manager);
+
+        adapter = new CategoriesRecyclerViewAdapter(this);
+        recycler.setAdapter(adapter);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AddCategoryActivity.class);
+                intent.putExtra("categoryId", (String) null);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
---
+++ app/src/main/java/pl/jansmi/scheduler/adapters/CategoriesRecyclerViewAdapter.java
@@ -0,0 +1,52 @@
+package pl.jansmi.scheduler.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import pl.jansmi.scheduler.App;
+import pl.jansmi.scheduler.R;
+import pl.jansmi.scheduler.dbstructure.entities.Category;
+
+public class CategoriesRecyclerViewAdapter extends RecyclerView.Adapter<ListItemViewHolder> {
+
+    private Context context;
+    private List<Category> categories;
+
+    public CategoriesRecyclerViewAdapter(Context context) {
+        this.context = context;
+        this.categories = App.db.categories().getAll();
+    }
+
+    @NonNull
+    @Override
+    public ListItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.listitem_main, null);
+        return new ListItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ListItemViewHolder holder, int position) {
+        Category category = categories.get(position);
+        holder.title.setText(category.getName());
+        holder.desc.setText(String.valueOf(category.getOrder()));
+        holder.menuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO: inflate menu
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return categories.size();
+    }
+}
