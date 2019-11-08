4f7f2390c194d7310e284481966ac6429a93cafb
jansmi <janek.smielowski@gmail.com>
2019-11-08 19:39:26 +0100

    DeletePromptDialog implemented

--- app/src/main/java/pl/jansmi/scheduler/dialogs/DeletePromptDialog.java
+++ app/src/main/java/pl/jansmi/scheduler/dialogs/DeletePromptDialog.java
@@ -1,15 +1,25 @@
 package pl.jansmi.scheduler.dialogs;
 
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.os.Parcelable;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 public class DeletePromptDialog extends DialogFragment {
-    public DeletePromptDialog() {
 
-
+    public static void show(Context context, DialogInterface.OnClickListener listener) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Confirm delete");
+        builder.setMessage("Are you sure you want to delete this record?");
+        builder.setPositiveButton("Yes", listener);
+        builder.setNegativeButton("No", null);
+        builder.create().show();
     }
-
-
 
 }
